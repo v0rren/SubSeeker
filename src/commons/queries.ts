4ac73c084f2f15827764d1ae2a4a23a77d1b1d38
CVona <77diana1992@gmail.com>
2023-09-10 22:46:22 +0200

    update app to use only the back end form making api calls

--- src/commons/queries.ts
+++ src/commons/queries.ts
@@ -1,79 +1,13 @@
-// External imports
 import axios from 'axios';
 
-// Local imports
-import SearchBar from "../components/SearchBar";
+const BASE_URL = 'http://localhost:3000/subreddit';
 
-// Define the structure of a Post
-export type Post = {
-    title: string;
-    author: string;  // The author of the post
-};
-
-/**
- * Searches for subreddits by their name.
- * @param subreddit - The name of the subreddit.
- * @returns An array of search results.
- */
-export async function searchSubreddits(subreddit: string): Promise<any[]> {
+export async function getRecommendedSubreddits(subreddit: string): Promise<any[]> {
     try {
-        const response = await axios.get(`https://www.reddit.com/subreddits/search.json?q=${subreddit}&limit=100`);
-
-        // Return the search result data directly.
+        const response = await axios.get(`${BASE_URL}/getRecommendedSubreddits/${subreddit}`);
         return response.data;
     } catch (error) {
-        console.error('Error searching subreddits:', error);
-        throw error;
-    }
-}
-
-/**
- * Searches for posts within a specific subreddit.
- * @param subreddit - The subreddit to search within.
- * @param searchType - The type of search (e.g., 'new', 'hot', etc.)
- * @returns An array of post authors.
- */
-export async function searchSubredditPosts(subreddit: string, searchType: string): Promise<string[]> {
-    try {
-        const res = await axios.get(`https://www.reddit.com/r/${subreddit}/${searchType}.json?limit=50`);
-
-        // Extract post details from the response
-        const posts: Post[] = res.data.data.children.map((child: any) => ({
-            title: child.data.title,
-            author: child.data.author,  // Extracting the author of the post
-        }));
-
-        // Extract and return just the authors from the posts
-        const authors = posts.map(post => post.author);
-        return authors;
-
-    } catch (error) {
-        console.error('Error searching subreddit post:', error);
-        throw error;
-    }
-}
-
-/**
- * Searches for subreddits where a specific user has made posts.
- * @param user - The username to search for.
- * @returns An array of subreddit names.
- */
-export async function searchSubredditsByUser(user: string): Promise<string[]> {
-    try {
-        const res = await axios.get(`https://www.reddit.com/user/${user}/submitted.json`);
-
-        // 1. Extract subreddit names from the user's comments
-        const subRedditNames: string[] = res.data.data.children.map((x: any) => x.data.subreddit);
-
-        // 2. Convert to a Set to remove duplicates
-        const uniqueSubRedditsSet: Set<string> = new Set(subRedditNames);
-
-        // 3. Convert the Set back to an array for the final result
-        const subReddits: string[] = Array.from(uniqueSubRedditsSet);
-        return subReddits;
-
-    } catch (error) {
-        console.error('Error searching subreddit post:', error);
+        console.error('Error getting recommended subreddits:', error);
         throw error;
     }
 }
--- src/components/RecommendedSubRedditList.tsx
+++ src/components/RecommendedSubRedditList.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import {
-    Post,
-    searchSubredditPosts,
-    searchSubreddits,
-    searchSubredditsByUser
+    getRecommendedSubreddits,
 } from "../commons/queries";
 import RecommendedSubRedditTable from "./RecommendedSubRedditTable";
 
@@ -20,59 +17,19 @@ export const RecommendedSubRedditList = () => {
         if (subReddit.trim() !== '') {
             const fetchData = async () => {
                 try {
-                    // Fetch top authors for the given subreddit
-                    let topAuthors = await searchSubredditPosts(subReddit, 'hot');
-
-                    // Fetch subreddits by each top author
-                    const subRedditMatrix = await Promise.all(
-                        topAuthors.map(author => searchSubredditsByUser(author))
-                    );
-
-                    // Object to track the occurrences and author overlap of each subreddit
-                    const subRedditOccurrencesMap: { [key: string]: { count: number, authorOverlap: number } } = {};
-
-                    // Populate the occurrences map
-                    subRedditMatrix.forEach((list, authorIndex) => {
-                        let uniqueSubredditsForAuthor = new Set<string>();  // To ensure we don't double-count for one author
-
-                        list.forEach((str) => {
-                            if(str.toLowerCase() !== subReddit.toLowerCase() && !uniqueSubredditsForAuthor.has(str)) {
-                                if (!subRedditOccurrencesMap[str]) {
-                                    subRedditOccurrencesMap[str] = { count: 1, authorOverlap: 1 };
-                                } else {
-                                    subRedditOccurrencesMap[str].count += 1;
-                                    subRedditOccurrencesMap[str].authorOverlap += 1;
-                                }
-                                uniqueSubredditsForAuthor.add(str);
-                            }
-                        });
-                    });
-
-                    // Convert authorOverlap into a percentage
-                    const totalAuthors = topAuthors.length;
-                    Object.values(subRedditOccurrencesMap).forEach(value => {
-                        value.authorOverlap = (value.authorOverlap / totalAuthors) * 100;
-                    });
-
-                    // Order the subreddits by their occurrences
-                    const orderedSubRedditOccurrences = Object.entries(subRedditOccurrencesMap).sort(
-                        (a, b) => b[1].count - a[1].count
-                    ).map(([subreddit, data]) => ({ subreddit, occurrence: data.count, authorOverlap: data.authorOverlap }));
-
-                    // Set the ordered occurrences to state
-                    setSubRedditOccurrences(orderedSubRedditOccurrences);
-
+                    // Fetch recommended subreddits using the new API endpoint
+                    const recommendedSubReddits = await getRecommendedSubreddits(subReddit);
+                    setSubRedditOccurrences(recommendedSubReddits);
                 } catch (error) {
                     console.error("There was an error fetching the data", error);
                 }
             };
 
-            // Invoke the fetchData function
             fetchData().catch(error => {
                 console.error("Unexpected error in fetchData:", error);
             });
         }
-    }, [subReddit]); // The effect depends on the subReddit state
+    }, [subReddit]);
 
     return (
         <div>
